013ecd0ef9814dabe1333038218955fcc9dd0095
Brandon <brandonkanute@gmail.com>
2023-09-19 03:18:07 +0300

    1.1: course information, step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = ({ course }) => {
+	return (
+		<div>
+			<h1>{course}</h1>
+		</div>
+	);
+};
 
-function App() {
-  const [count, setCount] = useState(0)
+const Content = ({
+	part1,
+	part2,
+	part3,
+	excercises1,
+	excercises2,
+	excercises3,
+}) => {
+	return (
+		<div>
+			<p>
+				{part1} {excercises1}
+			</p>
+			<p>
+				{part2} {excercises2}
+			</p>
+			<p>
+				{part3} {excercises3}
+			</p>
+		</div>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const Total = ({ excercises1, excercises2, excercises3 }) => {
+	return (
+		<div>
+			<p>Number of excercies {excercises1 + excercises2 + excercises3}</p>
+		</div>
+	);
+};
 
-export default App
+const App = (props) => {
+	const course = "Half Stack Application Development";
+	const part1 = "Fundamentals Of React";
+	const excercises1 = 10;
+	const part2 = "Using props to pass data";
+	const excercises2 = 7;
+	const part3 = "State of a component";
+	const excercises3 = 14;
+
+	return (
+		<div>
+			<Header course={course} />
+			<Content
+				part1={part1}
+				part2={part2}
+				part3={part3}
+				excercises1={excercises1}
+				excercises2={excercises2}
+				excercises3={excercises3}
+			/>
+			<Total
+				excercises1={excercises1}
+				excercises2={excercises2}
+				excercises3={excercises3}
+			/>
+		</div>
+	);
+};
+
+export default App;
